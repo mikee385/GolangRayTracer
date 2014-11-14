0cfc11a619bcaf929bb3abd98d529d1adce3ed54
mikee385 <mikee385@gmail.com>
2014-11-14 13:33:26 -0600

    Initial commit. (IN PROGRESS)
    This initial commit roughs out some of the basic concepts of Go so that
    I have examples to use when implementing the rest of the code.

---
+++ GolangRayTracer/table/Table.go
@@ -0,0 +1,40 @@
+package table
+
+import "fmt"
+
+type Table struct {
+	data   []interface{}
+	width  int
+	height int
+}
+
+func New(width int, height int) Table {
+	return Table{width: width, height: height}
+}
+
+func (table *Table) GetWidth() int {
+	return table.width
+}
+
+func (table *Table) GetHeight() int {
+	return table.height
+}
+
+func (table *Table) Set(row int, column int, value interface{}) {
+	table.data[table.getIndex(row, column)] = value
+}
+
+func (table *Table) Get(row int, column int) interface{} {
+	return table.data[table.getIndex(row, column)]
+}
+
+func (table *Table) getIndex(row int, column int) int {
+	if row >= table.height {
+		panic(fmt.Sprintf("Table::get_index: `row` overflow (%v >= %v)", row, table.height))
+	}
+	if column >= table.width {
+		panic(fmt.Sprintf("Table::get_index: `column` overflow (%v >= %v)", column, table.width))
+	}
+
+	return row*table.width + column
+}
