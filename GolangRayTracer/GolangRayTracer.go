8941ce967f40ffce1ea7ebc9416a4546a0d96a42
mikee385 <mikee385@gmail.com>
2014-11-14 13:52:11 -0600

    Updates Table to allocate storage.
    Updates the Table struct to allocate memory for the data and adds
    testing of Table to main.

--- GolangRayTracer/GolangRayTracer.go
+++ GolangRayTracer/GolangRayTracer.go
@@ -3,15 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/mikee385/GolangRayTracer/GolangRayTracer/color"
+	"github.com/mikee385/GolangRayTracer/GolangRayTracer/table"
 )
 
 func main() {
-	red := color.New(1.0, 0.0, 0.0)
-	green := color.New(0.0, 1.0, 0.0)
-	result := red.Add(green)
+	var red = color.New(1.0, 0.0, 0.0)
+	var green = color.New(0.0, 1.0, 0.0)
+	var result = red.Add(green)
 	fmt.Println(result.Red, result.Green, result.Blue)
 
 	fmt.Println(color.New(0.5, 0.5, 0.5))
 
 	fmt.Println(color.White())
+
+	var table = table.New(8, 6)
+	for row := 0; row < table.GetHeight(); row++ {
+		for column := 0; column < table.GetWidth(); column++ {
+			table.Set(row, column, color.Green())
+		}
+	}
+	for row := 0; row < table.GetHeight(); row++ {
+		for column := 0; column < table.GetWidth(); column++ {
+			fmt.Printf("%v, %v = %v\n", row, column, table.Get(row, column))
+		}
+	}
 }
