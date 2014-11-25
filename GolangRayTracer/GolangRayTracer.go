4d3806f13e04a483ad582552332b0432f67f785d
mikee385 <mikee385@gmail.com>
2014-11-25 16:13:56 -0600

    Updates `main` to create the first scene spheres.

--- GolangRayTracer/GolangRayTracer.go
+++ GolangRayTracer/GolangRayTracer.go
@@ -1,30 +1,63 @@
 package main
 
 import (
-	"fmt"
 	"github.com/mikee385/GolangRayTracer/GolangRayTracer/color"
-	"github.com/mikee385/GolangRayTracer/GolangRayTracer/table"
+	"github.com/mikee385/GolangRayTracer/GolangRayTracer/geometry"
+	"github.com/mikee385/GolangRayTracer/GolangRayTracer/material"
+	"github.com/mikee385/GolangRayTracer/GolangRayTracer/scene"
 )
 
 func main() {
-	var red = color.New(1.0, 0.0, 0.0)
-	var green = color.New(0.0, 1.0, 0.0)
-	var result = red.Add(green)
-	fmt.Println(result.Red, result.Green, result.Blue)
+	var width = 640
+	var height = 480
 
-	fmt.Println(color.New(0.5, 0.5, 0.5))
+	var groundSphere = scene.NewSphere(geometry.NewPoint(0.0, -10004.0, 20.0), 10000.0, material.NewBuilder().
+		Color(color.New(0.2, 0.2, 0.2)).
+		Diffuse(1.0).
+		Specular(0.0).
+		Shininess(0).
+		Reflection(0.0).
+		Refraction(0.0).
+		RefractiveIndex(0.0).
+		ToMaterial())
 
-	fmt.Println(color.White())
+	var sphere1 = scene.NewSphere(geometry.NewPoint(0.0, 0.0, 20.0), 4.0, material.NewBuilder().
+		Color(color.New(1.0, 0.32, 0.36)).
+		Diffuse(1.0).
+		Specular(0.0).
+		Shininess(0).
+		Reflection(1.0).
+		Refraction(0.5).
+		RefractiveIndex(1.1).
+		ToMaterial())
 
-	var table = table.New(8, 6)
-	for row := 0; row < table.GetHeight(); row++ {
-		for column := 0; column < table.GetWidth(); column++ {
-			table.Set(row, column, color.Green())
-		}
-	}
-	for row := 0; row < table.GetHeight(); row++ {
-		for column := 0; column < table.GetWidth(); column++ {
-			fmt.Printf("%v, %v = %v\n", row, column, table.Get(row, column))
-		}
-	}
+	var sphere2 = scene.NewSphere(geometry.NewPoint(5.0, -1.0, 15.0), 2.0, material.NewBuilder().
+		Color(color.New(0.9, 0.76, 0.46)).
+		Diffuse(1.0).
+		Specular(0.0).
+		Shininess(0).
+		Reflection(1.0).
+		Refraction(0.0).
+		RefractiveIndex(0.0).
+		ToMaterial())
+
+	var sphere3 = scene.NewSphere(geometry.NewPoint(5.0, 0.0, 25.0), 3.0, material.NewBuilder().
+		Color(color.New(0.65, 0.77, 0.97)).
+		Diffuse(1.0).
+		Specular(0.0).
+		Shininess(0).
+		Reflection(1.0).
+		Refraction(0.0).
+		RefractiveIndex(0.0).
+		ToMaterial())
+
+	var sphere4 = scene.NewSphere(geometry.NewPoint(-5.5, 0.0, 15.0), 3.0, material.NewBuilder().
+		Color(color.New(0.90, 0.90, 0.90)).
+		Diffuse(1.0).
+		Specular(0.0).
+		Shininess(0).
+		Reflection(1.0).
+		Refraction(0.0).
+		RefractiveIndex(0.0).
+		ToMaterial())
 }
