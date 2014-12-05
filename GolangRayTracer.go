7548dd099a634a533f1c040b499186be803dc280
mikee385 <mikee385@gmail.com>
2014-12-05 11:06:53 -0600

    Completes to `render` function of `main`.

--- GolangRayTracer.go
+++ GolangRayTracer.go
@@ -8,6 +8,7 @@ import (
 	"github.com/mikee385/GolangRayTracer/material"
 	"github.com/mikee385/GolangRayTracer/scene"
 	"github.com/mikee385/GolangRayTracer/table"
+	"math"
 )
 
 var EXAMPLE_TO_RUN = 1
@@ -249,9 +250,72 @@ func render(imageScene *scene.Scene, camera *scene.Camera) *table.Table {
 
 	for row := 0; row < height; row++ {
 		for column := 0; column < width; column++ {
-			pixelTable.Set(row, column, color.Green())
+			var ray = camera.PrimaryRay(row, column)
+			var result = imageScene.Trace(ray, 0)
+
+			var resultColor = color.New(
+				float32(math.Min(float64(result.Color.Red), 1.0)),
+				float32(math.Min(float64(result.Color.Green), 1.0)),
+				float32(math.Min(float64(result.Color.Blue), 1.0)))
+
+			pixelTable.Set(row, column, resultColor)
+		}
+	}
+
+	var isEdge = table.New(width, height)
+	isEdge.Fill(false)
+	for row := 1; row < height-1; row++ {
+		for column := 1; column < width-1; column++ {
+			var p1 = pixelTable.Get(row-1, column-1).(color.ColorRGB)
+			var p2 = pixelTable.Get(row-1, column).(color.ColorRGB)
+			var p3 = pixelTable.Get(row-1, column+1).(color.ColorRGB)
+			var p4 = pixelTable.Get(row, column-1).(color.ColorRGB)
+			var p6 = pixelTable.Get(row, column+1).(color.ColorRGB)
+			var p7 = pixelTable.Get(row+1, column-1).(color.ColorRGB)
+			var p8 = pixelTable.Get(row+1, column).(color.ColorRGB)
+			var p9 = pixelTable.Get(row+1, column+1).(color.ColorRGB)
+
+			var r = calculateGradient(p1.Red, p2.Red, p3.Red, p4.Red, p6.Red, p7.Red, p8.Red, p9.Red)
+			var g = calculateGradient(p1.Green, p2.Green, p3.Green, p4.Green, p6.Green, p7.Green, p8.Green, p9.Green)
+			var b = calculateGradient(p1.Blue, p2.Blue, p3.Blue, p4.Blue, p6.Blue, p7.Blue, p8.Blue, p9.Blue)
+
+			if r+b+g > 0.5 {
+				isEdge.Set(row, column, true)
+			} else {
+				isEdge.Set(row, column, false)
+			}
+		}
+	}
+
+	var subWidth = 3
+	var subHeight = 3
+	var subSize = subWidth * subHeight
+	var invSubSize = 1.0 / float32(subSize)
+	var subRays = table.New(subWidth, subHeight)
+	for row := 0; row < height; row++ {
+		for column := 0; column < width; column++ {
+			if isEdge.Get(row, column).(bool) {
+				var pixelColor = color.Black()
+
+				camera.SubRays(row, column, &subRays)
+				for subRow := 0; subRow < subHeight; subRow++ {
+					for subColumn := 0; subColumn < subWidth; subColumn++ {
+						var result = imageScene.Trace(subRays.Get(subRow, subColumn).(geometry.Ray3D), 0)
+
+						pixelColor = pixelColor.Add(result.Color.Scale(invSubSize))
+					}
+				}
+
+				pixelTable.Set(row, column, pixelColor)
+			}
 		}
 	}
 
 	return &pixelTable
+}
+
+func calculateGradient(p1 float32, p2 float32, p3 float32, p4 float32, p6 float32, p7 float32, p8 float32, p9 float32) float32 {
+	var gx = (p3 + 2*p6 + p9) - (p1 + 2*p4 + p7)
+	var gy = (p1 + 2*p2 + p3) - (p7 + 2*p8 + p9)
+	return float32(math.Sqrt(float64(gx*gx + gy*gy)))
 }
